fd1e88098bdc3a0100c3127544ecbfd2cd47cde3
Kean <wkeanphilip@gmail.com>
2026-08-04 17:20:55 +0800

    Remove ignored files and update gitignore

--- dist/config/env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENV = void 0;
-require("dotenv/config");
-exports.ENV = {
-    PORT: process.env.PORT,
-    DB_URL: process.env.DB_URL,
-};
