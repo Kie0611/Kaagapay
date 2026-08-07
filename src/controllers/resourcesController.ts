8fe1bce078b07ac91421b92a0cec8e9d405306f0
Kean <wkeanphilip@gmail.com>
2026-08-07 16:51:40 +0800

    Refactor getAllResources controller to accept request and response parameters; implement error handling and response formatting

--- src/controllers/resourcesController.ts
+++ src/controllers/resourcesController.ts
@@ -1,8 +1,38 @@
-export const getAllResources = async () => {
+import { Request, Response } from "express";
+import * as queries from "../db/queries"
+import { Category, Cost } from "../db/schema";
+import { ResourceParams } from "../types/resource.types";
 
-}
+export const getAllResources = async (req: Request, res: Response) => {
+  try {
+    const { category, city, barangay, cost, q } = req.query;
 
-export const getResourceById = async () => {
+    const resources = await queries.getAllResources({
+      category: category as Category,
+      city:     city as string,
+      barangay: barangay as string,
+      cost:     cost as Cost,
+      q:        q as string,
+    });
 
-}
+    res.status(200).json(resources);
+  } catch (error) {
+    console.log("Failed to get resources:", error);
+    res.status(500).json({ error: "Error getting resources"});
+  }
+};
+
+export const getResourceById = async (req: Request<ResourceParams>, res: Response) => {
+  try {
+    const { id } = req.params;
+    const resource = await queries.getResourceById(id);
+    
+    if(!resource) return res.status(404).json({ error: "Resource not found" });
+
+    res.status(200).json(resource)
+  } catch (error) {
+    console.log("Failed to get resource", error);
+    res.status(500).json({ error: "Error getting resource" });
+  }
+};
 
