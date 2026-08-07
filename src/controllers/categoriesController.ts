934401922192713a90b03680f71a40ffca808af8
Kean <wkeanphilip@gmail.com>
2026-08-07 16:51:28 +0800

    Implement getCategoriesWithCount controller with error handling and response

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,3 +1,12 @@
-export const getCategoriesWithCount = async () => {
-  
-}
+import { Request, Response } from "express";
+import * as queries from "../db/queries"
+
+export const getCategoriesWithCount = async (req: Request, res: Response) => {
+  try {
+    const categories = await queries.getCategoriesWithCount();
+    res.status(200).json(categories);
+  } catch (error) {
+    console.log("Failed to get categories:", error);
+    res.status(500).json({ error: "Error getting categories"});
+  }
+};
